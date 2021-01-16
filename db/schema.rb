dab5d0e5df3d1cb3eeb45f1c7840cbd33f14ae6b
Albert Chae <albertchae@users.noreply.github.com>
2021-01-16 12:28:31 -0800

    Implement tagging items with partner groups

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_30_233349) do
+ActiveRecord::Schema.define(version: 2021_01_16_200129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -289,6 +289,15 @@ ActiveRecord::Schema.define(version: 2020_12_30_233349) do
     t.index ["short_name"], name: "index_organizations_on_short_name"
   end
 
+  create_table "partner_group_items", force: :cascade do |t|
+    t.bigint "partner_group_id", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_partner_group_items_on_item_id"
+    t.index ["partner_group_id"], name: "index_partner_group_items_on_partner_group_id"
+  end
+
   create_table "partner_group_memberships", force: :cascade do |t|
     t.bigint "partner_group_id"
     t.bigint "partner_id"
@@ -433,6 +442,8 @@ ActiveRecord::Schema.define(version: 2020_12_30_233349) do
   add_foreign_key "kits", "organizations"
   add_foreign_key "manufacturers", "organizations"
   add_foreign_key "organizations", "account_requests"
+  add_foreign_key "partner_group_items", "items"
+  add_foreign_key "partner_group_items", "partner_groups"
   add_foreign_key "partner_group_memberships", "partner_groups"
   add_foreign_key "partner_group_memberships", "partners"
   add_foreign_key "partner_groups", "organizations"
