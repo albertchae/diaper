b485714107be3bbb81b5438b1b8a3ed4f962aa34
Edwin Mak <edwinthinks@gmail.com>
2021-01-17 08:37:55 -0500

    Got a first integration test to work!

--- spec/system/partners/log_in_system_spec.rb
+++ spec/system/partners/log_in_system_spec.rb
@@ -10,53 +10,44 @@ RSpec.describe "Authentication", type: :system, js: true do
       end
 
       it 'should take the user to the partners dashboard' do
-        fill_in "user_email", with: partner_user.email
-        fill_in "user_password", with: password
+        fill_in "partner_user_email", with: partner_user.email
+        fill_in "partner_user_password", with: password
         find('input[name="commit"]').click
 
         expect(current_path).to eq(partners_dashboard_path)
       end
     end
 
-    context 'when attempting to access diaperbase as a partner user' do
-      before do
-        visit new_user_session_path
+    context 'failing to login because' do
+      context 'due to attempting to login to diaperbase as a partner user' do
+        before do
+          visit new_user_session_path
+        end
+
+        it 'should show a error message and stay on the login page' do
+          fill_in "user_email", with: partner_user.email
+          fill_in "user_password", with: password
+          find('input[name="commit"]').click
+
+          expect(page).to have_content("Invalid Email or password")
+        end
       end
 
-      it 'should not grant access' do
-        visit "/users/sign_in"
-        fill_in "user_email", with: "deactivated@example.com"
-        fill_in "user_password", with: "password"
-        find('input[name="commit"]').click
+      context 'giving invalid credentials' do
+        before do
+          visit partner_user_session_path
+        end
 
-        expect(page).to have_content("Invalid Email or password")
+        it 'should show a error message and stay on the login page' do
+          fill_in "partner_user_email", with: partner_user.email
+          fill_in "partner_user_password", with: 'not-the-right-password'
+          find('input[name="commit"]').click
+
+          expect(page).to have_content("Invalid Email or password")
+
+          expect(current_path).to eq(partner_user_session_path)
+        end
       end
-    end
-
-
-  end
-
-  describe "Success" do
-    it "should show dashboard upon signin" do
-      sign_in(@user)
-
-      visit "/"
-      expect(page.find("h1")).to have_content "Dashboard"
-    end
-  end
-
-  describe "Deactivated user" do
-    before do
-      create(:user, :deactivated, email: "deactivated@exmaple.com")
-    end
-
-    it "should not allow the user to log in" do
-      visit "/users/sign_in"
-      fill_in "user_email", with: "deactivated@example.com"
-      fill_in "user_password", with: "password"
-      find('input[name="commit"]').click
-
-      expect(page).to have_content("Invalid Email or password")
     end
   end
 end
